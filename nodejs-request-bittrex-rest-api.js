86db2e0d29c3308417b08d8f21d4f61b59388243
Jason Clay <anticlergygang@gmail.com>
2017-10-16 04:26:04 -0400

    i gooberd it, but now its un gooberd.

--- nodejs-request-bittrex-rest-api.js
+++ nodejs-request-bittrex-rest-api.js
@@ -1,14 +1,13 @@
 const request = require('request');
-const bittrexRequest = (method, query = '', version = 'v1.1', apikey = '', secret = '') => {
-    query = query.concat('&');
+const bittrexRequest = (method, query = '', version = 'v1.1', secret = '') => {
     return new Promise((resolve, reject) => {
-        let url = `https://bittrex.com/api/${version}/${method}?${query}apikey=${apikey}&nonce=${(new Date().getTime)}`;
+        let url = `https://bittrex.com/api/${version}/${method}?${query}&nonce=${(new Date().getTime)}`;
         request.get({
             url: url,
             headers: {
                 'User-Agent': 'request',
                 'content-type': 'application/json',
-                'apisign': crypto.createHmac('sha512', Buffer(secret, 'base64')).update(url).digest('base64')
+                'apisign': crypto.createHmac('sha512', secret).update(url).digest('hex')
             }
         }, (err, body, res) => {
             if (err) {
@@ -21,6 +20,30 @@ const bittrexRequest = (method, query = '', version = 'v1.1', apikey = '', secre
                     reject(jsonRes.message);
                 }
             }
+        });
+    });
+};
+const getMarketTickersPromise = () => {
+    return new Promise((resolve, reject) => {
+        bittrexRequest('public/getmarkets').then(markets => {
+            let tickerPromises = [];
+            markets.forEach((market, marketIndex) => {
+                if (market.IsActive) {
+                    tickerPromises.push(`${markets[marketIndex].BaseCurrency}-${markets[marketIndex].MarketCurrency}`);
+                    tickerPromises.push(bittrexRequest('public/getticker', `market=${markets[marketIndex].BaseCurrency}-${markets[marketIndex].MarketCurrency}`));
+                }
+            });
+            return Promise.all(tickerPromises);
+        }).then(markets => {
+            let jsonMarkets = {};
+            let timestamp = (new Date().getTime());
+            for (let i = 0; i < markets.length; i = i + 2) {
+                markets[i + 1].timestamp = timestamp;
+                jsonMarkets[markets[i]] = markets[i + 1];
+            }
+            resolve(jsonMarkets);
+        }).catch(err => {
+            reject(err);
         });
     });
 };
